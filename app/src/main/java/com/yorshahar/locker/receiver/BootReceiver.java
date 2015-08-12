20d68006a21888d8896b2e7f54e6a4fb3e83ba30
yorshahar <yaniv.orshahar@gmail.com>
2015-08-12 15:06:33 -0400

    Created the BootReceiver.

---
+++ app/src/main/java/com/yorshahar/locker/receiver/BootReceiver.java
@@ -0,0 +1,32 @@
+package com.yorshahar.locker.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.yorshahar.locker.service.MyService;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    public BootReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        switch (intent.getAction()) {
+            case Intent.ACTION_USER_PRESENT: {
+                context.startService(new Intent(context, MyService.class));
+                break;
+            }
+            case Intent.ACTION_BOOT_COMPLETED: {
+                context.startService(new Intent(context, MyService.class));
+                break;
+            }
+            default: {
+
+            }
+        }
+
+    }
+
+}
