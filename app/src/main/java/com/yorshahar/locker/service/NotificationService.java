024babb59a61310f9393de4b511839329ae6648f
yorshahar <yaniv.orshahar@gmail.com>
2015-08-12 00:33:26 -0400

    Notification Service.

--- app/src/main/java/com/yorshahar/locker/service/NotificationService.java
+++ app/src/main/java/com/yorshahar/locker/service/NotificationService.java
@@ -2,40 +2,25 @@ package com.yorshahar.locker.service;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
-import android.content.Intent;
 import android.view.accessibility.AccessibilityEvent;
 
 public class NotificationService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        // TODO Auto-generated method stub.
-//Code when the event is caught
         int a=2;
     }
 
     @Override
     public void onInterrupt() {
-        // TODO Auto-generated method stub.
-        int a=3;
-
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        int a=2;
     }
 
     @Override
     protected void onServiceConnected() {
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.feedbackType = AccessibilityServiceInfo.DEFAULT;
-        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
         info.notificationTimeout = 100;
         setServiceInfo(info);
     }
